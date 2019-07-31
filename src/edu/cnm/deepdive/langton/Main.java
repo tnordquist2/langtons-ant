b4c891fa84cdcd82277de44efe0e75d06bdf272b
Todd A Nordquist <toddnordquist144@gmail.com>
2019-07-31 09:29:03 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/langton/Main.java
@@ -0,0 +1,44 @@
+package edu.cnm.deepdive.langton;
+
+import edu.cnm.deepdive.langton.controller.MainController;
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  private static final String RESOURCES_DIR = "resources";
+  private static final String LAYOUT_RESOURCE = RESOURCES_DIR + "/main.fxml";
+  private static final String RESOURCE_BUNDLE_BASE_NAME = RESOURCES_DIR + "/strings";
+  private static final String WINDOW_TITLE_KEY = "window_title";
+
+  private MainController controller = new MainController();
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    ClassLoader classLoader = getClass().getClassLoader();
+    ResourceBundle bundle = ResourceBundle.getBundle(RESOURCE_BUNDLE_BASE_NAME);
+    FXMLLoader fxmlLoader = new FXMLLoader(classLoader.getResource(LAYOUT_RESOURCE), bundle);
+    Parent root = fxmlLoader.load();
+    controller = fxmlLoader.getController();
+    Scene scene = new Scene(root);
+    stage.setTitle(bundle.getString(WINDOW_TITLE_KEY));
+    // TODO Set other elements of the scene from our resources.
+    stage.setResizable(false);
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  @Override
+  public void stop() throws Exception {
+    // TODO Stop any controllers.
+    super.stop();
+  }
+
+}
---
+++ src/edu/cnm/deepdive/langton/controller/MainController.java
@@ -0,0 +1,18 @@
+package edu.cnm.deepdive.langton.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.control.ToggleButton;
+
+public class MainController {
+
+  @FXML
+  private ToggleButton runToggle;
+
+  @FXML
+  private Slider populationSize;
+
+  public void toggleRun(ActionEvent actionEvent) {
+  }
+}
